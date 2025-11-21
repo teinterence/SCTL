7bc26b568676a677aaedb72b4d9306fede03d8f5
terencesyu <teinterence@gmail.com>
2025-11-21 19:11:38 +0800

    更新替換辭庫

--- workflow.bat
+++ workflow.bat
@@ -1,3 +1,3 @@
-.\.venv\Scripts\python.exe opencc_s2t.py -i global_chs.ini -o global_cht.ini -m s2twp
-.\.venv\Scripts\python.exe term_replace.py -i global_cht.ini -o global_cht.ini -v
-powershell -Command "$existing = Get-Content global_cht.ini -Encoding UTF8; $append = Get-Content coded_traditional.ini -Encoding UTF8; ($existing + $append) | Set-Content global_cht.ini -Encoding UTF8"
+.\.venv\Scripts\python.exe opencc_s2t.py -i global_chs.ini -o global.ini -m s2twp
+.\.venv\Scripts\python.exe term_replace.py -i global.ini -o global.ini -v
+powershell -Command "$existing = Get-Content global.ini -Encoding UTF8; $append = Get-Content coded_traditional.ini -Encoding UTF8; ($existing + $append) | Set-Content global.ini -Encoding UTF8"
